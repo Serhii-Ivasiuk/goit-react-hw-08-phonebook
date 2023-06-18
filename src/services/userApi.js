cefaabae2902edad8bdfca87fa1061a7241dec20
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-06-18 18:28:25 +0300

    Update userApi.js

--- src/services/userApi.js
+++ src/services/userApi.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 export const signUp = user =>
-  axios.post('/users/signup', user).then(res => res.data);
+  axios.post('/users/signup', user).then(response => response.data);
 
 export const signIn = user =>
-  axios.post('/users/login', user).then(res => res.data);
+  axios.post('/users/login', user).then(response => response.data);
 
 export const signOut = token =>
-  axios.post('/users/logout', token).then(res => res.data);
+  axios.post('/users/logout', token).then(response => response.data);
 
 export const getCurrentUser = () =>
-  axios.get('/users/current').then(res => res.data);
+  axios.get('/users/current').then(response => response.data);

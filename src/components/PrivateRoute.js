fe4479ceecdbd01c953b4bb1c3edae2d173a0c2c
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-06-21 21:20:06 +0300

    added private route isRefreshing check

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,10 +2,13 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 // Redux selectors
-import { getIsLoggedIn } from 'redux/auth/authSelectors';
+import { getIsLoggedIn, getIsRefreshing } from 'redux/auth/authSelectors';
 
 export const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
   const isLoggedIn = useSelector(getIsLoggedIn);
+  const isRefreshing = useSelector(getIsRefreshing);
 
-  return isLoggedIn ? Component : <Navigate to={redirectTo} />;
+  const shouldRedirect = !isRefreshing && !isLoggedIn;
+
+  return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
 };

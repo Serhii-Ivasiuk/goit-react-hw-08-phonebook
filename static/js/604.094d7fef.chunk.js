9b3edd88e4235eac9677b695e8d7e76bf13e9fbf
Serhii-Ivasiuk <111089930+Serhii-Ivasiuk@users.noreply.github.com>
2023-06-25 13:08:12 +0000

    Deploying to gh-pages from @ Serhii-Ivasiuk/goit-react-hw-08-phonebook@d4e8c6415b5f5d411af2c03989f6e6e33143fdf6 🚀

---
+++ static/js/604.094d7fef.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[604],{5604:function(n,e,t){t.r(e),t.d(e,{InfoMessage:function(){return u},List:function(){return h}});var o,i,c=t(168),r=t(4934),u=r.Z.p(o||(o=(0,c.Z)(["\n  height: 48px;\n  font-weight: 700;\n  text-align: center;\n  color: #505050;\n"]))),h=r.Z.ul(i||(i=(0,c.Z)(["\n  display: flex;\n  flex-direction: column;\n  gap: 16px;\n"])))}}]);
+//# sourceMappingURL=604.094d7fef.chunk.js.map
--- static/js/604.77f6d7a9.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[604],{5604:function(n,e,t){t.r(e),t.d(e,{InfoMessage:function(){return u},List:function(){return a}});var o,c,i=t(168),r=t(4934),u=r.Z.p(o||(o=(0,i.Z)(["\n  font-weight: 700;\n  text-align: center;\n  color: #505050;\n"]))),a=r.Z.ul(c||(c=(0,i.Z)(["\n  display: flex;\n  flex-direction: column;\n  gap: 16px;\n"])))}}]);
-//# sourceMappingURL=604.77f6d7a9.chunk.js.map
